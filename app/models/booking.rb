bc046c0260f750fd395b71a8ab954549b9cf2547
Arno Fleming <arno.fleming@gmail.com>
2017-03-19 20:52:12 +0100

    Styling matters

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,21 +4,21 @@ class Booking < ApplicationRecord
 
   before_create :set_check_in_times
 
-  def self.during arrival, departure
+  def self.during(arrival, departure)
     arrival = arrival.change(hour: 14, min: 00, sec: 00)
     departure = departure.change(hour: 11, min: 00, sec: 00)
     starts_before_ends_after(arrival, departure).or( ends_during(arrival, departure) ).or(starts_during(arrival,departure))
   end
 
-  def self.starts_before_ends_after arrival, departure
+  def self.starts_before_ends_after(arrival, departure)
     #TODO: where statements
   end
 
-  def self.starts_during arrival, departure
+  def self.starts_during(arrival, departure)
     #TODO: where statements
   end
 
-  def self.ends_during arrival, departure
+  def self.ends_during(arrival, departure)
     #TODO: where statements
   end
 
@@ -32,12 +32,10 @@ class Booking < ApplicationRecord
     room.available? starts_at, ends_at
   end
 
-
   private
+
   def set_check_in_times
     self.starts_at = starts_at.change(hour: 14, min: 00, sec: 00)
     self.ends_at = ends_at.change(hour: 11, min: 00, sec: 00)
   end
-
-
 end
