34888b1cbb84fcba8fbcba6ee058900e3e730881
konstantin0s <constantintofan85@gmail.com>
2017-12-18 20:13:35 +0100

    Update API::RoomsController to use ActiveModelSerializers

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
+gem 'active_model_serializers', '~> 0.10.0'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 3.2'
 
---
+++ app/serializers/room_serializer.rb
@@ -0,0 +1,8 @@
+class RoomSerializer < ActiveModel::Serializer
+  attributes :id
+
+  attributes :home_type, :room_type, :accommodate, :bedroom_count, :bathroom_count, :listing_name, :description, :address, :has_tv, :has_kitchen, :has_airco, :has_heating, :has_internet, :price
+
+ has_many :bookings
+ 
+end
